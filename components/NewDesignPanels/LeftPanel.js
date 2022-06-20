d2be077cbe536c40589bc9ed341dc0eb9ed98a24
vishal1145 <vishal.kumar1145@gmail.com>
2022-06-20 18:43:49 +0700

    fixed in stoneshape dispatching value null on div select

--- components/NewDesignPanels/LeftPanel.js
+++ components/NewDesignPanels/LeftPanel.js
@@ -101,7 +101,7 @@ const LeftPanel = ({ props }) => {
   // }
 
   const setStoneShape = e => {
-    console.log(e.target)
+    if(e.target.tagName!=='IMG') return
     dispatch(designProps({ ...currDesign, currStoneShape: e.target.alt }))
   }
 
